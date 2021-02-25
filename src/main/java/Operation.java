bbb93c8d7f7f636b6a2644e7d1aef203e4316479
zigabait123321 <65978599+zigabait123321@users.noreply.github.com>
2021-02-25 23:37:54 +0300

    second commit

--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -1,9 +1,30 @@
-public class Operation {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+
+public class Operation {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String userWrite;
+        while (true) {
+            userWrite = reader.readLine();
+
+            String[] arrayValue = userWrite.split(" ");
+
+            String strFirst = arrayValue[0];
+            String operation = arrayValue[1];
+            String strSecond = arrayValue[2];
+
+            int valueFirst = Integer.parseInt(strFirst);
+            int valueSecond = Integer.parseInt(strSecond);
+
+            System.out.println(valueFirst + " " + valueSecond + " " + operation);
+        }
     }
 
     void execute() {
-
+        System.out.println("null");
     }
 }
